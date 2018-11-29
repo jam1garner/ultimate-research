578fb627ecde61a35a65e495deefd8a39693cdf1
jam1garner <jam1.mcleod@hotmail.com>
2018-11-29 15:50:23 -0300

    Add nus3audio extractor

---
+++ extract_nus3audio.py
@@ -0,0 +1,27 @@
+import struct
+
+def uint32(f):
+    return struct.unpack('<L', f.read(4))[0]
+
+def readString(myfile):
+    chars = []
+    while True:
+        c = myfile.read(1)
+        if c == b'\x00':
+            return b''.join(chars).decode('ascii')
+        chars.append(c)
+
+with open('nus3_found.csv', 'r') as f:
+    offsets = [int(i.split(',')[0].rstrip('h'), 16) for i in f.readlines()]
+
+with open('data.arc', 'rb') as f:
+    for offset in offsets:
+        f.seek(offset + 4)
+        fileSize = uint32(f) + 8
+        isNusaudio = (f.read(4) == b'AUDI')
+        f.seek(offset + (0x48 if isNusaudio else 0xAD))
+        fileName = readString(f)
+        fullFileName = "{}_{}.{}".format(fileName, hex(offset), "nus3audio" if isNusaudio else "nus3bank")
+        f.seek(offset)
+        with open('nus3audio_uncompressed/'+fullFileName, 'wb') as out:
+            out.write(f.read(fileSize))
